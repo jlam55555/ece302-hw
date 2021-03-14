b7ae48496270d68e4fcc3d98ddc8aafadb6b68a5
Jonathan Lam <jlam55555@gmail.com>
2021-03-14 00:34:16 -0500

    added more figures for proj2

--- proj2/proj2.m
+++ proj2/proj2.m
@@ -2,6 +2,7 @@
 % (L)MMSE estimators
 % Jonathan Lam & Steven Lee
 clc; clear; close all;
+set(0, 'defaultTextInterpreter', 'latex');
 
 %% 1a
 N = 1e6;
@@ -28,7 +29,8 @@ Y_err = (Y - Y_est).^2;
 Y_X_err = sortrows([X Y_err]);
 Y_X_err(:,2) = movmean(Y_X_err(:,2), N/1e2);
 
-figure();
+figure('Position', [0 0 1000 1000]);
+tiledlayout(1, 1, 'TileSpacing', 'tight');
 hold on;
 
 % plot all X vs error data
@@ -38,12 +40,17 @@ scatter(X, Y_err);
 x = -3:1e-2:-1;
 plot(x, (3+x).^2/12, 'k');
 x = -1:1e-2:1;
-plot(x, 1/3*ones(size(x)), 'k');
+plot(x, 1/3*ones(size(x)), 'k', 'HandleVisibility', 'off');
 x = 1:1e-2:3;
-plot(x, (3-x).^2/12, 'k');
+plot(x, (3-x).^2/12, 'k', 'HandleVisibility', 'off');
 
 % plot empirical piecewise MSE
 plot(Y_X_err(:,1), Y_X_err(:,2), 'r');
+
+legend({'Squared Error', 'Theoretical MSE', 'Empirical MSE (moving average)'});
+xlabel('Observed $$X$$');
+ylabel('$$(\hat{Y}(X)-Y)^2$$');
+title('Error in estimate vs. observed $$X$$');
 
 % calculate empirical MSE
 MSE_emp = mean(Y_err);
@@ -59,28 +66,61 @@ display(table([1/4; 4/15], [MSE_emp; LMSE_emp], ...
 
 %% 2
 clear;
-N = 1e6;
+N = 1e4;
 mu_y = 1;
-sig_ys = [0.1 1 10];
-sig_rs = [0.1 1 10];
-R = 10;		% R is the number of output variables
+sig_ys = 1:10;
+sig_rs = 1:10;
+Rs = 1:10;		% R is the number of observations
 
+% calculate all da results
 res = [];
 row_labels = {};
 i = 1;
-for sig_y = sig_ys
-	for sig_r = sig_rs
-		[emp, theo] = calc_llmse(mu_y, sig_y, sig_r, N, R);
-		emp = string(sprintf('%0.4f', emp));
-		theo = string(sprintf('%0.4f', theo));
-		res(i, :) = [emp, theo];
-		row_labels{i} = sprintf('sig_y=%.1f, sig_r=%.1f', sig_y, sig_r);
-		i = i + 1;
-	end
+for sig_y_ind = 1:length(sig_ys)
+for sig_r_ind = 1:length(sig_rs)
+for R_ind = 1:length(Rs)
+	sig_y = sig_ys(sig_y_ind);
+	sig_r = sig_rs(sig_r_ind);
+	R = Rs(R_ind);
+	[emp, theo] = calc_llmse(mu_y, sig_y, sig_r, N, R);
+	res(R_ind, sig_r_ind, sig_y_ind, :) = [emp, theo];
+	row_labels{i} = sprintf('sig_y=%.2f, sig_r=%.2f, R=%d', sig_y, sig_r, R);
+	i = i + 1;
 end
-display(table(res(:,1), res(:,2), ...
-	'VariableNames', {'Theoretical', 'Empirical'}, 'RowNames', row_labels))
+end
+end
 
+% plotting slices of the result vector
+figure('Position', [0 0 1200 400]);
+tiledlayout(1, 3, 'TileSpacing', 'tight');
+% plot MSE vs R
+nexttile();
+mvr = reshape(res(:, 5, 5, :), [], 2);
+plot(Rs, mvr(:,1), Rs, mvr(:,2));
+xlabel('Number of observations');
+ylabel('LMMSE');
+title('LMMSE vs. Number of observations ($$\sigma_y=\sigma_r=5$$)');
+% plot MSE vs sig_y
+nexttile();
+mvsy = reshape(res(5, :, 5, :), [], 2);
+plot(sig_ys, mvsy(:,1), sig_ys, mvsy(:,2));
+xlabel('$$\sigma_y$$');
+ylabel('LMMSE');
+title('LMMSE vs. $$\sigma_y$$ ($$R=\sigma_r=5$$)');
+% plot MSE vs sig_r
+nexttile();
+mvsr = reshape(res(5, 5, :, :), [], 2);
+plot(sig_rs, mvsr(:,1), sig_rs, mvsr(:,2));
+xlabel('$$\sigma_r$$');
+ylabel('LMMSE');
+title('LMMSE vs. $$\sigma_r$$ ($$R=\sigma_y=5$$)');
+
+% displaying results in a table
+res = reshape(res, [], 2);
+display(table(res(:,1), res(:,2), ...
+	'VariableNames', {'Empirical', 'Theoretical'}, 'RowNames', row_labels))
+
+% function to help with calculations at different values of the parameters
 function [emp, theo] = calc_llmse(mu_y, sig_y, sig_r, N, R)
 	Y_gaus = mu_y + sig_y * randn(N, 1);
 	R_gaus = sig_r * randn(N, R);
