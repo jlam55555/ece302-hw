63617794da830d53962f68404d20689fed7ce2bb
Jonathan Lam <jlam55555@gmail.com>
2021-03-13 23:33:51 -0500

    finished project 2

--- proj2/proj2.m
+++ proj2/proj2.m
@@ -1,4 +1,7 @@
-% clc; clear; close all;
+% ECE302 Proj 2
+% (L)MMSE estimators
+% Jonathan Lam & Steven Lee
+clc; clear; close all;
 
 %% 1a
 N = 1e6;
@@ -8,87 +11,95 @@ W = 4*rand(N, 1) - 2;
 
 X = Y + W;
 
+% piecewise estimators
 X1 = X < -1;
 X2 = (X >= -1) & (X <= 1);
 X3 = X > 1;
-
 Y1 = (1 + X(X1)) / 2;
 Y2 = zeros(size(X(X2)));
 Y3 = (-1 + X(X3)) / 2;
 
-YYhat = [Y(X1) Y1; Y(X2) Y2; Y(X3) Y3];
+X = [X(X1); X(X2); X(X3)];
+Y = [Y(X1); Y(X2); Y(X3)];
+Y_est = [Y1; Y2; Y3];
+Y_err = (Y - Y_est).^2;
 
-Xs = [X(X1); X(X2); X(X3)];
-Ys = (YYhat(:,1) - YYhat(:,2)).^2;
-
-alldata = [Xs Ys];
-alldata = sortrows(alldata);
-alldataxs = alldata(:,1);
-alldatays = alldata(:,2);
-% alldata = reshape(alldata, 100, []);
-% alldata = mean(alldata, 1);
-alldatays = movmean(alldatays, N/1e2);
+% for plotting empirical piecewise MSE as a function of X
+Y_X_err = sortrows([X Y_err]);
+Y_X_err(:,2) = movmean(Y_X_err(:,2), N/1e2);
 
 figure();
 hold on;
-scatter(Xs, Ys);
+
+% plot all X vs error data
+scatter(X, Y_err);
+
+% plot piecewise MSE
 x = -3:1e-2:-1;
 plot(x, (3+x).^2/12, 'k');
 x = -1:1e-2:1;
 plot(x, 1/3*ones(size(x)), 'k');
 x = 1:1e-2:3;
 plot(x, (3-x).^2/12, 'k');
-plot(alldataxs, alldatays, 'r');
-% plot(linspace(-3, 3, length(alldata)), alldata, 'r');
 
-MSE = mean((YYhat(:,1) - YYhat(:,2)).^2)
+% plot empirical piecewise MSE
+plot(Y_X_err(:,1), Y_X_err(:,2), 'r');
+
+% calculate empirical MSE
+MSE_emp = mean(Y_err);
 
 %% 1b
-Ylin = X/5;
+Y_lin = X/5;
+LMSE_emp = mean((Y_lin - Y).^2);
 
-LMSE = mean((Ylin - Y).^2)
+%table making
+display(table([1/4; 4/15], [MSE_emp; LMSE_emp], ...
+	'VariableNames', {'Theoretical', 'Empirical'}, ...
+	'RowNames', {'MMSE', 'LMMSE'}))
 
 %% 2
-muy = 1;
-sigy = 8;
-sigr = 5;
-R = 10;
+clear;
+N = 1e6;
+mu_y = 1;
+sig_ys = [0.1 1 10];
+sig_rs = [0.1 1 10];
+R = 10;		% R is the number of output variables
 
-Ygaus = muy + sigy * randn(N, 1);
-Rgaus = sigr * randn(N, R);
+res = [];
+row_labels = {};
+i = 1;
+for sig_y = sig_ys
+	for sig_r = sig_rs
+		[emp, theo] = calc_llmse(mu_y, sig_y, sig_r, N, R);
+		emp = string(sprintf('%0.4f', emp));
+		theo = string(sprintf('%0.4f', theo));
+		res(i, :) = [emp, theo];
+		row_labels{i} = sprintf('sig_y=%.1f, sig_r=%.1f', sig_y, sig_r);
+		i = i + 1;
+	end
+end
+display(table(res(:,1), res(:,2), ...
+	'VariableNames', {'Theoretical', 'Empirical'}, 'RowNames', row_labels))
 
-Xgaus = Ygaus + Rgaus;
+function [emp, theo] = calc_llmse(mu_y, sig_y, sig_r, N, R)
+	Y_gaus = mu_y + sig_y * randn(N, 1);
+	R_gaus = sig_r * randn(N, R);
+	X = Y_gaus + R_gaus;
 
-% solve matrix equation Ax=b
-% general case: 8.70; less general case: 8.77
-% 8.77
+	% solve normal equation C_XX*a=C_XY (8.70)
+	C_XX = sig_y^2 * ones(R) + sig_r^2 * eye(R);
+	C_XY = sig_y^2 * ones(R, 1);
+	a = C_XX^-1 * C_XY;
 
-% sig^2_X1 = var(y+r) = var(y) + var(r) = sigy^2 + sigr^2
-% sig_X1X2 = E[(X1-mu_x1)*(X2-mu_x2)] = E[X1X2] - muY^2 = E[Y^2]-mu_Y^2 = var(Y)
+	% 8.74: comes from requirement that Yhat_lin is unbiased
+	% (same as 8.60 assuming that mu_x=mu_y since mu_r=0)
+	a0 = mu_y * (1 - sum(a));
 
-% E[X1x2]=E[(Y+R1)(Y+R2)]=E[Y^2+YR_1+Y_R2+R_1R_2]=E[Y^2]
+	Y_gaus_est = a0 + sum(a.' .* X, 2);
 
-% diagonal terms are all : sigy^2+sigr^2
-% all other terms are : sigy^2
+	% empirical MSE
+	emp = mean((Y_gaus_est - Y_gaus).^2);
 
-% XiXj i != j
-
-% A = [sigy^2+sigr^2 sigy^2; sigy^2 sigy^2+sigr^2]; 
-% b = sigy^2 * ones(R, 1);
-% a = A^-1 * b;
-
-% 8.70
-C_XX = sigy^2 * ones(R) + sigr^2 * eye(R);
-C_XY = sigy^2 * ones(R, 1);
-a = C_XX^-1 * C_XY;
-
-% 8.74: comes from requirement that Yhat_lin is unbiased
-% (same as 8.60 assuming that mu_x = mu_y since mu_r=0)
-a0 = muy * (1 - sum(a));
-
-Yhat = a0 + sum(a.' .* Xgaus, 2);
-MSE = mean((Yhat - Ygaus).^2)
-
-% 8.73: associated MMSE
-sigy^2 - C_XY.'*(C_XX)^-1*C_XY
-
+	% 8.73: theoretical associated MSE
+	theo = sig_y^2 - C_XY.' * C_XX^-1 * C_XY;
+end
