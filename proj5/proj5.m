540d80a6b1c50d47908c49f96791f25fcf487a04
Jonathan Lam <jlam55555@gmail.com>
2021-05-14 18:55:41 -0400

    did proj5

--- proj5/proj5.m
+++ proj5/proj5.m
@@ -1,17 +1,75 @@
-c[n] = [1 .2 .4]
+%% ECE302 Project 5
+%% Stevel Lee & Jonathan Lam
 
-s[n] = 2*randi(2)-3
-d[n] = sigma * randn()
+clc;
+clear;
+close all;
 
-s_hat[n] = ((s[n] * c[n]) + d[n]) * h[n]
+%% Part 1
 
-Find R_sr[n], R_rr[n]
+%                        d[n]
+%                         |
+%         +------+        v        +------+
+% s[n] -> | c[n] | ----> (+) ----> | h[n] | -> shat[n]
+%         +------+  x[n]      r[n] +------+
+%
+% Calculations: (note: * means convolution)
+%
+% R_ss[n] = E[s[m]s[m+n]] = { 1   if n=0 } = delta[n]
+%                           { 0   else   }
+%
+% Use eq. 11.8 and fact that s, d are uncorrelated:
+% R_rs[n] = R_xs[n] = R_ss[n] * c[n] = c[n]
+%
+% Use fact that s, d are uncorrelated:
+% R_rr[n] = R_xx[n] + R_dd[n]
+% 
+% Autocorrelation of gaussian white noise:
+% R_dd[n] = sigma^2 * delta[n]
+% (we set sigma^2=1)
+%
+% 
 
-R_sr[n] = c[n] * R_ss[n]
+% We have tried to replicate the system using the autocorrelation
+% (Rrr) as well as cross-correlation (Rsr)
 
-r[n] = ((s[n] * c[n]) + d[n])
+% So we used the xcorr function is matlab 
+% (xcorr(conv(s, c, 'same') + d, s, 4, 'biased')) 
+% and the results seems right, however, the mse is off the mark. 
+% The accuracy look to be on mark, which even though the MSE does not
 
-R_sr[n] = E[s(n)r(n-n0)] = E[s(n)(s[n-n0] * c[n-n0] + d[n-n0])]
-= E[s(n)s(n-n0) * c[n-n0]] + E[s(n)d(n-n0)]
-= E[S(n)s(n-n0)] * c[n]
-= R_ss[n] * c[n]
+
+
+
+
+N = 10;
+sig2 = 1;
+
+% R_rr = [1.2+sig2 .28 .4 0 0 0 0 0 0 0];
+R_rr = [1.2 .28 .4 0 0 0 0 0 0 0];
+R = R_rr(abs((1:N) - (1:N).') + 1)
+R_sr = [1 .2 .4 0 0 0 0 0 0 0].'
+
+h = [0; 0; 0; 0; 0; 0; 0; 0; 0; (R \ R_sr(1:N))]
+
+% h = [.7754; -.1410; -.305; .1175]
+
+%% Part 2
+M = 1e6;
+s = 2*randi(2, [M 1]) - 3;
+d = sqrt(sig2) * randn([M 1]);
+
+c = [0 0 1 .2 .4];
+
+r = conv(s, c, 'same'); % + d;
+s_hat = conv(r, h, 'same');
+
+mse = mean((s-s_hat).^2)
+
+
+s(1:10)
+d(1:10)
+
+rnge = 100:200;
+[s(rnge) sign(s_hat(rnge)) (s(rnge) == sign(s_hat(rnge))) s_hat(rnge)]
+[mean((s(rnge) == sign(s_hat(rnge)))) mse]
