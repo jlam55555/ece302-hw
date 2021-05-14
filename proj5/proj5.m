414b471ae0c2bc05d10fda76d7dac1ecb84c8694
Jonathan Lam <jlam55555@gmail.com>
2021-05-14 19:26:42 -0400

    finished proj5

--- proj5/proj5.m
+++ proj5/proj5.m
@@ -1,5 +1,5 @@
 %% ECE302 Project 5
-%% Stevel Lee & Jonathan Lam
+%% Steven Lee & Jonathan Lam
 
 clc;
 clear;
@@ -28,48 +28,65 @@ close all;
 % R_dd[n] = sigma^2 * delta[n]
 % (we set sigma^2=1)
 %
-% 
+% based on Wiener-Khinchin theorem:
+% R_xx[n] = R_ss[n] * R_cc[n]
+% where R_cc[n] = autocorrelation of c[n] = c*c
 
-% We have tried to replicate the system using the autocorrelation
-% (Rrr) as well as cross-correlation (Rsr)
-
-% So we used the xcorr function is matlab 
-% (xcorr(conv(s, c, 'same') + d, s, 4, 'biased')) 
-% and the results seems right, however, the mse is off the mark. 
-% The accuracy look to be on mark, which even though the MSE does not
-
-
-
-
-
-N = 10;
-sig2 = 1;
-
-% R_rr = [1.2+sig2 .28 .4 0 0 0 0 0 0 0];
-R_rr = [1.2 .28 .4 0 0 0 0 0 0 0];
-R = R_rr(abs((1:N) - (1:N).') + 1)
-R_sr = [1 .2 .4 0 0 0 0 0 0 0].'
-
-h = [0; 0; 0; 0; 0; 0; 0; 0; 0; (R \ R_sr(1:N))]
-
-% h = [.7754; -.1410; -.305; .1175]
+% We have tried to replicate the autocorrelation calculation
+% R_xy[n] using the xcorr function xcorr(x, y, N, 'biased'))
+% and the autocorrelations seems correct.
 
 %% Part 2
+
+% arbitrary choose a value for sigma^2
+sig2 = 0.5;
+
+% M is number of samples
 M = 1e6;
+
+% create random vectors
 s = 2*randi(2, [M 1]) - 3;
 d = sqrt(sig2) * randn([M 1]);
 
+% need to pad c so that convolution with
+% type "same" works correctly
 c = [0 0 1 .2 .4];
 
-r = conv(s, c, 'same'); % + d;
-s_hat = conv(r, h, 'same');
+% precompute r -- doesn't depend on h
+r = conv(s, c, 'same') + d;
 
-mse = mean((s-s_hat).^2)
+% N is the length of the filter h
+for N = [4 6 10]
+	% set up normal equations
+	R_rr = zeros([N, 1]);
+	R_rr(1:3) = [1.2+sig2 .28 .4];
+	
+	R_sr = zeros([N, 1]);
+	R_sr(1:3) = [1 .2 .4].';
 
+	R = R_rr(abs((1:N) - (1:N).') + 1)
 
-s(1:10)
-d(1:10)
+	% solve normal equation
+	% Rh = R_sr => h = inv(R)*R_sr
+	h = R \ R_sr(1:N)
 
-rnge = 100:200;
-[s(rnge) sign(s_hat(rnge)) (s(rnge) == sign(s_hat(rnge))) s_hat(rnge)]
-[mean((s(rnge) == sign(s_hat(rnge)))) mse]
+	% need to pad h so that it's correctly centered
+	% so that conv w/ "same" padding works correctly
+	h = [zeros([N-1 1]); h];
+
+	% calculate estimate with our filter
+	s_hat = conv(r, h, 'same');
+
+	% calculate and print mse
+	mse = mean((s-s_hat).^2);
+	acc = mean(sign(s) == sign(s_hat));
+	fprintf('N=%d: MSE=%f; accuracy=%f\n', N, mse, acc);
+end
+
+% For some reason, increasing N does not improve MSE. However, the
+% MSE is fairly low, and the accuracy (determined by the relative
+% frequency that s_hat predicts the correct sign of s) is definitely
+% above random guessing (0.5). This may be because the coefficients
+% of h[n] for n>4 are relatively small, so increasing N does not
+% improve performance. However, changing sigma^2 affects performance
+% as expected.
